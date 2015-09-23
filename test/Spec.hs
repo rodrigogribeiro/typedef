fe45a46ed7bfb1eb2b9d34eef4039b2d1eb5deb3
Rodrigo <rodrigogribeiro@gmail.com>
2015-09-23 11:04:27 -0300

    updated test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,9 +10,10 @@ tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
 unitTests = testGroup "Unit tests"
-               [ testCase "Solver test for T0" $ execATest "./test/cases/T0.ctr",
-                 testCase "Solver test for T1" $ execATest "./test/cases/T1.ctr",
-                 testCase "Solver test for T2" $ execATest "./test/cases/T2.ctr"]
+               [ testCase "Solver test for T0" $ execATest "./test/cases/T0.c",
+                 testCase "Solver test for T1" $ execATest "./test/cases/T1.c",
+                 testCase "Solver test for T2" $ execATest "./test/cases/T2.c",
+                 testCase "Solver test for T2" $ execATest "./test/cases/T3.c"]
 
 
 execATest file = do               
