607287ecde9b149449194be00e5d3c845b63786e
Rodrigo <rodrigogribeiro@gmail.com>
2015-09-25 06:45:54 -0300

    More changes on test spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,7 +13,8 @@ unitTests = testGroup "Unit tests"
                [ testCase "Solver test for T0" $ execATest "./test/cases/T0.c",
                  testCase "Solver test for T1" $ execATest "./test/cases/T1.c",
                  testCase "Solver test for T2" $ execATest "./test/cases/T2.c",
-                 testCase "Solver test for T3" $ execATest "./test/cases/T3.c"]
+                 testCase "Solver test for T3" $ execATest "./test/cases/T3.c",
+                 testCase "Solver tesst for T4" $ execATest "./test/cases/T4.c"]
 
 
 execATest file = do               
